c44af16363b8ea846e12b0ec090ad76564bed36f
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-10-29 15:10:09 +0100

    write a bottom up solution

--- src/main/java/fax/play/CutStickBottomUp.java
+++ src/main/java/fax/play/CutStickBottomUp.java
@@ -1,7 +1,45 @@
 package fax.play;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class CutStickBottomUp {
+   private int n;
+   private int[] c;
 
+   public int minCost(int length, int[] cuts) {
+      Arrays.sort(cuts);
+      n = cuts.length + 1;
+      c = new int[n + 1];
+      c[0] = 0;
+      for (int i = 0; i < cuts.length; i++) {
+         this.c[i + 1] = cuts[i];
+      }
+      c[n] = length;
 
+      // memory can be optimized here
+      int[][] solutions = new int[n+1][n+1];
+      for (int size = 2; size <= n; size++) {
+         for (int left = 0; left <= n - size; left++) {
+            int right = left + size;
+            int currentLength = c[right] - c[left];
+            if (size == 2) {
+               // single cut =>  base case
+               solutions[left][right] = currentLength;
+            } else {
+               PriorityQueue<Integer> localSolutions = new PriorityQueue<>(size);
+               for (int k = left + 1; k <= right - 1; k++) {
+                  int iTOk = solutions[left][k];
+                  int kTOj = solutions[k][right];
 
+                  localSolutions.add(currentLength + iTOk + kTOj);
+               }
+               Integer solution = localSolutions.peek();
+               solutions[left][right] = solution;
+            }
+         }
+      }
+
+      return solutions[0][n];
+   }
 }
--- src/test/java/fax/play/CutStickTest.java
+++ src/test/java/fax/play/CutStickTest.java
@@ -6,10 +6,17 @@ import org.junit.jupiter.api.Test;
 public class CutStickTest {
 
    private CutStickMinimumCost testTarget = new CutStickMinimumCost();
+   private CutStickBottomUp bottomUp = new CutStickBottomUp();
 
    @Test
-   public void test() {
+   public void memoization() {
       Assertions.assertThat(testTarget.minCost(7, new int[]{1, 3, 4, 5})).isEqualTo(16);
       Assertions.assertThat(testTarget.minCost(9, new int[]{5, 6, 1, 4, 2})).isEqualTo(22);
+   }
+
+   @Test
+   public void bottomUp() {
+      Assertions.assertThat(bottomUp.minCost(7, new int[]{1, 3, 4, 5})).isEqualTo(16);
+      Assertions.assertThat(bottomUp.minCost(9, new int[]{5, 6, 1, 4, 2})).isEqualTo(22);
    }
 }
